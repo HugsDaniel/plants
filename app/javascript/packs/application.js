59823d9d39338df4a2b40b100a2179996ee76425
Hugo Daniel <hugodaniel.icam@gmail.com>
2020-04-03 09:00:44 +0200

    Livecode

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,10 +7,6 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("channels")
 
+import { initSelect2 } from "../components/init_select2"
 
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+initSelect2();
